bbf8280d1e5834eeff35b0eb1ad33f292d24d363
Emanuel Errante <emanuel_errante@hotmail.com>
2024-12-29 11:45:57 -0300

    Commit inicial con .gitignore correctamente configurado

---
+++ control-acceso-backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API funcionando 🚀');
+});
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ MongoDB conectado'))
+.catch(err => console.error('❌ Error al conectar MongoDB:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
